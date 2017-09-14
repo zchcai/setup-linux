22b62c5bf6cd2f2289e144caf9a926faf868818c
Zhicheng Cai <taxic@users.noreply.github.com>
2017-09-14 16:33:56 -0500

    Update and rename pthread_example.c to template.cpp

---
+++ template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(int i=(a); i<(b); i++)
+#define pb push_back
+#define mp make_pair
+#define X first
+#define Y second
+#define clr(a,v) memset(a,v,sizeof(a))
+#define sqr(x) (x)*(x)
+#define MOD 1000000007
+
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef vector<pair<int,int> > vpii;
+const int INF=0x3f3f3f3f;
+const LL INFL=0x3f3f3f3f3f3f3f3fLL;
+const double EPS=1e-8;
+const double PI = acos(-1.0);
+
+// start-----------------------------------
+int main(){
+
+	return 0;
+}
